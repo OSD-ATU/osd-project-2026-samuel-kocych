3726bed73ee526fa414bba5331d4062131a451d0
samuelkocych <samuel@kocych.cz>
2026-03-01 16:48:08 +0000

    fixed guards

--- frontend/src/app/guards/editor.guard.ts
+++ frontend/src/app/guards/editor.guard.ts
@@ -1,19 +1,25 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthCustomService } from '../services/auth-custom.service';
-import { inject } from '@angular/core';
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthCustomService } from "../services/auth-custom.service";
+import { inject } from "@angular/core";
 
 export const editorGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthCustomService);
   const router = inject(Router);
 
+  if (!authService.isAuthenticated$.value) {
+    return router.createUrlTree(["/login"], {
+      queryParams: { returnUrl: state.url },
+    });
+  }
+
   const role = authService.currentUser$.value?.role;
 
   // editor or admin
-  if (role === 'editor' || role === 'admin') {
+  if (role === "editor" || role === "admin") {
     return true;
   }
 
-  return router.createUrlTree(['/login'], {
+  return router.createUrlTree(["/login"], {
     queryParams: { returnUrl: state.url },
   });
 };
