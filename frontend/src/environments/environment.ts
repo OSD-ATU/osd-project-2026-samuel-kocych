4983e969fb4c2f74667c36a559a116d795568c2a
samuelkocych <samuel@kocych.cz>
2026-03-16 16:05:32 +0000

    activated cloudflare tunnel

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUri: "http://localhost:3000/api/v1",
+  apiUri:
+    "https://beginners-council-buildings-admitted.trycloudflare.com/api/v1",
   lambdaUri: "https://mh0dzyf5lj.execute-api.eu-west-1.amazonaws.com/dev",
   apiKey: "mysecretkey",
   googleAnalyticsId: "G-F0XZ2M471V",
