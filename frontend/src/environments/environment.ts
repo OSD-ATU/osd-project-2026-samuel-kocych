1a36bc87225615a5ce7305eca5d90bc978735489
samuelkocych <samuel@kocych.cz>
2026-03-17 11:36:21 +0000

    changed cloudlfare url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  apiUri:
-    "https://beginners-council-buildings-admitted.trycloudflare.com/api/v1",
+  apiUri: "https://fisher-buf-nevada-locking.trycloudflare.com",
   lambdaUri: "https://mh0dzyf5lj.execute-api.eu-west-1.amazonaws.com/dev",
   apiKey: "mysecretkey",
   googleAnalyticsId: "G-NELS8TCWZ1",
