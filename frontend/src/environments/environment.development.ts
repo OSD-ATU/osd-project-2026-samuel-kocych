ff599fdb4bf7fefb821c74de89c76f0aae5e5310
samuelkocych <samuel@kocych.cz>
2026-03-17 11:25:52 +0000

    removed unused files

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUri: "http://localhost:3000/api/v1",
   lambdaUri: "https://mh0dzyf5lj.execute-api.eu-west-1.amazonaws.com/dev",
   apiKey: "mysecretkey",
-  googleAnalyticsId: "G-F0XZ2M471V",
+  googleAnalyticsId: "G-NELS8TCWZ1",
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     "https://beginners-council-buildings-admitted.trycloudflare.com/api/v1",
   lambdaUri: "https://mh0dzyf5lj.execute-api.eu-west-1.amazonaws.com/dev",
   apiKey: "mysecretkey",
-  googleAnalyticsId: "G-F0XZ2M471V",
+  googleAnalyticsId: "G-NELS8TCWZ1",
 };
