a7bb0f68b9c563dc85b612c7a92d20955f68a4be
samuelkocych <samuel@kocych.cz>
2026-03-17 11:44:59 +0000

    fixed url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUri: "https://fisher-buf-nevada-locking.trycloudflare.com",
+  apiUri: "https://fisher-buf-nevada-locking.trycloudflare.com/api/v1",
   lambdaUri: "https://mh0dzyf5lj.execute-api.eu-west-1.amazonaws.com/dev",
   apiKey: "mysecretkey",
   googleAnalyticsId: "G-NELS8TCWZ1",
